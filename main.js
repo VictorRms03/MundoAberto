24c2d959ac6f9d4cc9f4ab6887b3f6f228876d87
Victor Ramos <victorrms03@gmail.com>
2024-12-21 11:00:52 -0300

    feat: criação do arquivo main.js e comentários sobre requisitos

---
+++ main.js
@@ -0,0 +1,16 @@
+import * as THREE from 'three';
+
+/*
+
+- Incluir diferentes tipos de luz. Com ajustes de intensidade, cor e efeitos, como sombras. (pelo menos 2 tipos de luzes)
+
+- Aplicação de texturas em superfícies para simular materiais variados (madeira, metal, pedra, entre outros), como bump map para aumentar o realismo.
+
+- Importação de pelo menos três modelos 3D em formatos comuns, como ‘.obj‘, e sua integração ao cenário.
+
+- Implementação de um sistema de câmera que permita ao usuário explorar o ambiente (movimentação em primeira ou terceira pessoa).
+
+- O mundo deve ter uma temática definida, como medieval, futurista, natural, ou outro escolhido pela equipe, com atenção aos detalhes visuais.
+
+*/
+

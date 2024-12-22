adf41a9facae61ba29e270f5cbbe3c9eea733751
Victor Ramos <victorrms03@gmail.com>
2024-12-22 02:29:26 -0300

    feat: modelo base

--- main.js
+++ main.js
@@ -1,16 +1,58 @@
 import * as THREE from 'three';
 
-/*
+/* Cena, Câmera e Renderizador */
 
-- Incluir diferentes tipos de luz. Com ajustes de intensidade, cor e efeitos, como sombras. (pelo menos 2 tipos de luzes)
+// Cena
+const scene = new THREE.Scene();
 
-- Aplicação de texturas em superfícies para simular materiais variados (madeira, metal, pedra, entre outros), como bump map para aumentar o realismo.
+// Câmera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+camera.position.y = 3;
+camera.position.z = 5;
 
-- Importação de pelo menos três modelos 3D em formatos comuns, como ‘.obj‘, e sua integração ao cenário.
+// Renderizador
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-- Implementação de um sistema de câmera que permita ao usuário explorar o ambiente (movimentação em primeira ou terceira pessoa).
 
-- O mundo deve ter uma temática definida, como medieval, futurista, natural, ou outro escolhido pela equipe, com atenção aos detalhes visuais.
 
-*/
 
+/* Luzes */
+// Luz ambiente
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+scene.add(ambientLight);
+
+/* Objetos */
+
+// Solo
+
+const groundGeometry = new THREE.PlaneGeometry( 4, 4 );
+const groundMaterial = new THREE.MeshPhongMaterial( { color: 0xBA8E23 } );
+groundMaterial.side = THREE.DoubleSide; //Apenas para renderizar o outro lado do piso
+const ground = new THREE.Mesh( groundGeometry, groundMaterial );
+
+ground.rotation.x = -Math.PI / 2;
+
+scene.add( ground );
+
+// Cubo para testes de luz
+
+const cubeGeometry = new THREE.BoxGeometry( 0.5, 0.5, 0.5 );
+const cubeMaterial = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
+
+cube.position.y = 1;
+
+scene.add( cube );
+/* Função de animação */
+function animar() {
+  requestAnimationFrame(animar);
+  
+  //cube.rotation.x += 0.05;
+  //cube.rotation.y += 0.05;
+
+  renderer.render(scene, camera);
+}
+
+// Inicia a animação
